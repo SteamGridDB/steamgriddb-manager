1c3bd6b13ae03c09636f10f94b4855a891cf4783
Jozen <internet.cafe.da@gmail.com>
2019-07-15 15:13:02 +0800

    List Origin games and save on check

--- src/js/Import.js
+++ src/js/Import.js
@@ -3,54 +3,123 @@ import React from 'react';
 import {Theme as UWPThemeProvider, getTheme} from "react-uwp/Theme";
 import Tabs, { Tab } from "react-uwp/Tabs";
 import TextBox from "react-uwp/TextBox";
+import ListView, { ListViewProps } from "react-uwp/ListView";
+import Separator from "react-uwp/Separator";
+import CheckBox from "react-uwp/CheckBox";
+import Toggle from "react-uwp/Toggle";
 import Spinner from './spinner.js';
 import Origin from "./Origin";
+import {crc32} from 'crc';
 
 class Import extends React.Component {
     constructor(props) {
         super(props);
 
+        this.store = new Store();
+
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            currentPlatform: null,
+            originGames: []
         };
     }
 
     componentDidMount() {
         Origin.isInstalled().then((installed) => {
             if (installed) {
-                Origin.getOriginGames().then((games) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: games
-                    });
-                });
+                this.fetchOrigin();
             }
         });
+    }
 
+    fetchOrigin() {
+        Origin.getGames().then((games) => {
+            this.setState({
+                isLoaded: true,
+                originGames: games
+            });
+        });
+    }
+
+    platformGameSave(platformName, game) {
+        this.store.set(`platforms.${platformName}.${crc32(game.id).toString(16)}`, game);
+    }
+
+    platformGameRemove(platformName, game) {
+        this.store.delete(`platforms.${platformName}.${crc32(game.id).toString(16)}`);
+    }
+
+    platformGameExists(platformName, game) {
+        return this.store.has(`platforms.${platformName}.${crc32(game.id).toString(16)}`);
+    }
+
+
+    onCheck(game, checked) {
+        if (checked) {
+            this.platformGameSave('origin', game);
+        } else {
+            this.platformGameRemove('origin', game);
+        }
+    }
+
+    gameList(games, platform) {
+        return (
+            games.map((game) => {
+                let checked = false;
+                if (this.platformGameExists(platform, game)) {
+                    checked = true;
+                }
+
+                return (
+                    <span key={game.id}>
+                        <CheckBox
+                            onCheck={this.onCheck.bind(this, game)}
+                            background='none'
+                            style={{width: '100%'}}
+                            labelPosition='left'
+                            label={game.name}
+                            defaultChecked={checked}
+                        />
+                    </span>
+                )
+            })
+        )
     }
 
     render() {
-        const {isLoaded} = this.state;
+        const {isLoaded, originGames} = this.state;
+        const listStyle = {
+            background: 'none',
+            border: 0
+        }
 
         if (!isLoaded) {
             return (<Spinner/>);
         }
 
         return (
-            <Tabs>
-                <Tab title="Origin">
-                    Origin
-                </Tab>
-                <Tab title="UPlay">
-                    UPlay
-                </Tab>
-                <Tab title="GOG">
-                    GOG
-                </Tab>
-                <Tab title="Epic Games Store">
-                    Epic Games Store
-                </Tab>
-            </Tabs>
+            <div>
+                <Tabs useAnimate={true} style={{width: '100%'}}>
+                    <Tab title="Origin" style={{width: '100%'}}>
+                        <div style={{overflowX: 'hidden', overflowY: 'auto', maxHeight: 'calc(100vh - 80px)'}}>
+                            <div style={{padding: 10}}>
+                                <p>Found {originGames.length} Origin Games.</p>
+                                <p>Choose games to import from Origin</p>
+                            </div>
+                            <ListView style={listStyle} listSource={this.gameList(originGames, 'origin')} />
+                        </div>
+                    </Tab>
+                    <Tab title="Uplay">
+                        Uplay
+                    </Tab>
+                    <Tab title="GOG.com">
+                        GOG.com
+                    </Tab>
+                    <Tab title="Epic Games Store">
+                        Epic Games Store
+                    </Tab>
+                </Tabs>
+            </div>
         );
     }
 }
--- src/js/Origin.js
+++ src/js/Origin.js
@@ -22,41 +22,33 @@ class Origin {
         });
     }
 
-    static getOriginGames() {
+    static getGames() {
         return new Promise((resolve, reject) => {
             let originDataPath = 'C:\\ProgramData\\Origin';
             let games = [];
-            if (fs.existsSync(originDataPath)) {
-                fs.readdir(path.join(originDataPath, 'LocalContent'), (err, folders) => {
-                    folders.forEach((folder) => {
-                        let manifestFolder = path.join(originDataPath, 'LocalContent', folder);
-                        if (fs.lstatSync(manifestFolder).isDirectory()) {
-                            fs.readdir(manifestFolder, (err, files) => {
-                                files.some((file) => {
-                                    // Get first file with .mfst extension
-                                    if (path.extname(file) === '.mfst') {
-                                        let manifestFile = path.join(originDataPath, 'LocalContent', folder, file);
-                                        
-                                        // .mfst file is just a text file with a query string
-                                        let manifestStr = fs.readFileSync(manifestFile).toString();
-                                        let manifestStrParsed = querystring.parse(manifestStr);
+            if (fs.existsSync(path.join(originDataPath, 'LocalContent'))) {
+                fs.readdirSync(path.join(originDataPath, 'LocalContent')).forEach((folder) => {
+                    let manifestFolder = path.join(originDataPath, 'LocalContent', folder);
+                    if (fs.lstatSync(manifestFolder).isDirectory()) {
+                        fs.readdirSync(manifestFolder).some((file) => {
+                            // Get first file with .mfst extension
+                            if (path.extname(file) === '.mfst') {
+                                let manifestFile = path.join(originDataPath, 'LocalContent', folder, file);
+                                
+                                // .mfst file is just a text file with a query string
+                                let manifestStr = fs.readFileSync(manifestFile).toString();
+                                let manifestStrParsed = querystring.parse(manifestStr);
 
-                                        games.push({
-                                            appid: manifestStrParsed.id,
-                                            name: path.basename(folder),
-                                            image: false,
-                                            imageURI: false,
-                                            type: 'game'
-                                        });
-                                        return true;
-                                    }
+                                games.push({
+                                    id: manifestStrParsed.id,
+                                    name: path.basename(folder)
                                 });
-                            });
-                        }
-                    });
+                                return true;
+                            }
+                        });
+                    }
                 });
             }
-
             resolve(games);
         });
     }
