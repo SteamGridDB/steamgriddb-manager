0394184706c329b89fa53c248fcedbbec55a2fa9
Jozen <internet.cafe.da@gmail.com>
2019-07-27 02:19:46 +0800

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/js/index.js",
-  mode: "development",
-  target: "node",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  output: {
-    path: path.resolve(__dirname, "public/"),
-    publicPath: "/public/",
-    filename: "bundle.js"
-  }
+    entry: './src/js/index.js',
+    mode: 'development',
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env'] }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'public/'),
+        publicPath: '/public/',
+        filename: 'bundle.js'
+    }
 };
