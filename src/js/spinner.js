a02d61f838388c2f9dc148833ae3ead07cd37022
Jozen <internet.cafe.da@gmail.com>
2019-08-14 03:42:54 +0800

    Replace broken spinner from react-uwp with react-desktop

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ProgressRing from 'react-uwp/ProgressRing';
+import PropTypes from 'prop-types';
+import {ProgressCircle} from 'react-desktop/windows';
 
 class Spinner extends React.Component {
     constructor(props) {
@@ -15,10 +16,11 @@ class Spinner extends React.Component {
                 width: '100%',
                 height: '100%'
             }}>
-                <ProgressRing size={100} />
+                <ProgressCircle size="100" color={this.context.theme.accent} />
             </div>
         );
     }
 }
 
+Spinner.contextTypes = { theme: PropTypes.object };
 export default Spinner;
