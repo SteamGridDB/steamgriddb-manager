c3a9e37374b9edcbd32ba6a85b75ec5b8986dbd4
Jozen <internet.cafe.da@gmail.com>
2019-11-29 04:04:36 +0800

    Lil bit of clean up

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const {app, globalShortcut, BrowserWindow} = require('electron');
-const {autoUpdater} = require('electron-updater');
+const { app, globalShortcut, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
 const log = require('electron-log');
 
 const path = require('path');
@@ -7,7 +7,7 @@ const url = require('url');
 
 autoUpdater.autoInstallOnAppQuit = true;
 
-log.catchErrors({showDialog: true});
+log.catchErrors({ showDialog: true });
 
 log.info(`Started SGDB Manager ${app.getVersion()}`);
 
@@ -15,41 +15,41 @@ log.info(`Started SGDB Manager ${app.getVersion()}`);
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
-    autoUpdater.checkForUpdatesAndNotify();
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        frame:false,
-        icon: path.join(__dirname, 'assets/icons/192x192.png'),
-        transparent: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+function createWindow() {
+  autoUpdater.checkForUpdatesAndNotify();
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    icon: path.join(__dirname, 'assets/icons/192x192.png'),
+    transparent: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'public', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'public', 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
 
-    // Open the DevTools.
-    globalShortcut.register('CommandOrControl+Shift+L', () => {
-        mainWindow.webContents.openDevTools();
-    });
+  // Open the DevTools.
+  globalShortcut.register('CommandOrControl+Shift+L', () => {
+    mainWindow.webContents.openDevTools();
+  });
 
-    mainWindow.on('beforeunload', () => {
-        globalShortcut.unregisterAll();
-    });
+  mainWindow.on('beforeunload', () => {
+    globalShortcut.unregisterAll();
+  });
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -59,17 +59,17 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
